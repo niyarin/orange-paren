c2c8294d4e0684144137e310fb1cd695a957c9bc
niyarin <inoanet@gmail.com>
2019-12-07 01:43:37 +0900

    Add support for saving previous execution result.

--- src/orange-paren.scm
+++ src/orange-paren.scm
@@ -7,19 +7,85 @@
      (scheme read))
    (export orange-paren-run)
    (begin 
+
+     (define %REPL-SETTINGS
+       '((prompt "orange-paren>")
+         (*3 '())
+         (*2 '())
+         (*1 '())
+         ))
+
+     (define (%set-orange-paren-settings! env port setting-set)
+       (for-each
+         (lambda (sym)
+           (let ((target
+                    (cond
+                      ((assv sym %REPL-SETTINGS)
+                       => cadr)
+                      (else '()))))
+             (when target
+               (eval `(define ,sym ,target) env))))
+         setting-set))
+
+     (define (%ref-prompt env)
+       (let ((prompt (eval 'prompt env)))
+         (cond
+           ((procedure? prompt)
+            (prompt))
+           ((string? prompt)
+            prompt)
+           (else
+             prompt))))
+
+     (define (%save-return-value! input return-value env)
+       (unless 
+         (or (eval `(eq? *1 ,input) env)
+             (eval `(eq? *2 ,input) env)
+             (eval `(eq? *3 ,input) env))
+          (eval '(set! *3 *2) env)
+          (eval '(set! *2 *1) env)
+          (eval `(set! *1 ,return-value) env)))
+
      (define (orange-paren-run . config)
        (call/cc
          (lambda (break)
           (let ((env (interaction-environment)))
+            (%set-orange-paren-settings! env (current-input-port) '(prompt *3 *2 *1))
             (let loop ()
-               (display ">>")
+               (display (%ref-prompt env))
                (flush-output-port)
                (let ((input (read)))
                  (when (eof-object? input)
                   (break))
                   
-                 (let ((r (eval input env)))
-                    (display r)(newline)
+                 (let ((r '()))
+                   (call/cc 
+                     (lambda (repl-error-break)
+                       (with-exception-handler
+                         (lambda (err-object)
+                           (set! r err-object)
+                           (repl-error-break #f ))
+                         (lambda ()
+                             (set! r
+                                  (eval 
+                                    `(call/cc 
+                                       (lambda (repl-error-break)
+                                         (with-exception-handler
+                                           (lambda (err)
+                                             (repl-error-break err))
+                                           (lambda ()
+                                             ,input))))
+                                    env))))))
+
+                    (cond 
+                      ((error-object? r)
+                        (display 
+                           (error-object-message r))(newline)
+                        (display 
+                           (error-object-irritants r))(newline))
+                      (else
+                       (display r)(newline)
+                       (%save-return-value! input r env)))
                     (loop))
                ))))))
      ))
